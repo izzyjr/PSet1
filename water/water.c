ea11ba4f7d3df5c942061e13c7e96dca3c6319a1
Israel Mesa <mesa.israel50@gmail.com>
2017-10-28 01:18:30 +0000

    Water

---
+++ water/water.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+printf("minutes: ");
+int minutes = get_int();
+
+int bottles = (minutes * 192)/ 16;
+printf("bottles: %i\n", bottles);
+}
