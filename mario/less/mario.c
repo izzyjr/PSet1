310906a7da4df35c9ebbca7468915c5fc70da1df
Israel Mesa <mesa.israel50@gmail.com>
2017-10-29 19:15:15 +0000

    Mario and Greedy-update

---
+++ mario/less/mario.c
@@ -0,0 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int hash;
+    int space;
+    int height;
+    do
+    {
+        printf("height: ");
+        height = get_int( );
+
+    }
+    while (height > 23 || height <0);
+
+   for(int nr = 0; nr < height; nr++)
+   {
+       for(space = 1; space < height - nr; space++)
+       {
+           printf(" ");
+       }
+
+       for(hash = -2; hash < height - space; hash++)
+       {
+            printf("#");
+       }
+       printf("\n");
+   }
+}
