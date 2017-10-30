c4a73cd0fa45c84b5d3458efac95849c6f0c2027
Israel Mesa <mesa.israel50@gmail.com>
2017-10-30 02:23:13 +0000

    Greedy - update and final

--- greedy/greedy.c
+++ greedy/greedy.c
@@ -1,7 +1,46 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
+
 {
-    printf("O hai! How much change is owed?");
+
+    int count = 0;
+    int penny = 1;
+    int nickle = 5;
+    int dime = 10;
+    int quarter = 25;
+    float input;
+    int change;
+    do
+    {
+        printf("How much change is owed? ");
+        input = get_float();
+        change = round(input * 100);
+    }
+    while (change <= 0);
+
+    while (change >= quarter)
+    {
+        change = change - quarter;
+        count++;
+    }
+    while (change >= dime)
+    {
+        change = change - dime;
+        count++;
+    }
+    while (change >= nickle)
+    {
+        change = change - nickle;
+        count++;
+    }
+    while (change >= penny)
+    {
+        change =  change - penny;
+        count++;
+    }
+    printf("%i\n", count);
+
 }
